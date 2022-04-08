7f44be48d5921dba7dde8a436a9c1f9bc8cd5108
Trần Văn Tài <90708185+tranvantai2905@users.noreply.github.com>
2022-04-08 20:21:44 +0700

    lan 2

--- src/queue.c
+++ src/queue.c
@@ -10,10 +10,23 @@ void enqueue(struct queue_t * q, struct pcb_t * proc) {
 	/* TODO: put a new process to queue [q] */	
 }
 
-struct pcb_t * dequeue(struct queue_t * q) {
-	/* TODO: return a pcb whose prioprity is the highest
+truct pcb_t * dequeue(struct queue_t * q) {
+	/* TODO: return a pcb whose priority is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	return NULL;
+	int index = 0;
+	if(q->size == 0) return NULL;
+	struct pcb_t * proc = q->proc[0];
+	for(int i = 1 ; i < q->size ; i++) {
+		if(proc->priority < q->proc[i]->priority) {
+			proc = q->proc[i];
+			index = i;
+		}
+	}
+	for(int i = index ; i < q->size - 1 ; i++) {
+		q->proc[i] = q->proc[i+1];
+	}
+	q->size--;
+	return proc;
 }
 
